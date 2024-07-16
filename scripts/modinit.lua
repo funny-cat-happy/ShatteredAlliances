d8aba1466f552780bd3b3f8f65a3b35606697bf2
PieCat <fanyingjiyun@foxmail.com>
2024-07-16 17:32:46 +0800

    添加警报器组件

--- scripts/modinit.lua
+++ scripts/modinit.lua
@@ -20,21 +20,34 @@ end
 -- local MOD_STRINGS = include(scriptPath .. "/strings")
 -- modApi:addStrings(dataPath, "SHATTEREDALLIANCES", MOD_STRINGS)
 -- end
-
 local function lateInit(modApi)
 	-- local serverdefs = include("modules/serverdefs")
 	-- serverdefs.GENERAL_MISSIONS.TERMINALS.icon = "gui/collaboration.png"
 	-- serverdefs.SITUATIONS.executive_terminals.ui.icon = "collaboration.png"
 	-- log:write(util.stringize(serverdefs.SITUATIONS.executive_terminals.ui.icon))
 	-- log:flush()
-	local scriptPath = modApi:getScriptPath()
-	include(scriptPath .. "/clientModify/mui")
+	-- local scriptPath = modApi:getScriptPath()
+	-- include(scriptPath .. "/clientModify/mui")
 	local stateMainMenu = include('states/state-main-menu')
 	local mui = include("mui/mui")
+	local screen
 	local oldLoad = stateMainMenu.onLoad
 	stateMainMenu.onLoad = function(self)
 		oldLoad(self)
-		local screen = mui.createScreen("test.lua")
+		screen = mui.createScreen("test.lua")
+		screen.binder.alarm.binder.alarmRing1:setVisible(false)
+		screen.binder.alarm.binder.alarmRing1:setColor(1, 0, 0, 1)
+		screen.binder.alarm.binder.alarmRing1:setAnim("idle")
+		screen.binder.alarm.binder.alarmRing1:setVisible(true)
+		screen.binder.alarm.binder.alarmRing1:getProp():setListener(KLEIAnim.EVENT_ANIM_END,
+			function(anim, animname)
+				if animname == "idle" then
+					screen.binder.alarm.binder.alarmRing1:setVisible(false)
+				end
+			end)
+		screen.binder.alarm.binder.trackerTxt:setText("5/9")
+		MOAILogMgr.log("aa")
+		MOAILogMgr.log(util.stringize(screen.binder.alarm.binder.alarmRing1._cont._prop))
 		mui.activateScreen(screen)
 	end
 end
