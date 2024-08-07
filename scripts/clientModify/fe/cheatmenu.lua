e2db993c7559ecb24d276d90390b80e82120a30f
PieCat <fanyingjiyun@foxmail.com>
2024-08-07 16:02:01 +0800

    利用sim库重写调试菜单

--- scripts/clientModify/fe/cheatmenu.lua
+++ scripts/clientModify/fe/cheatmenu.lua
@@ -1,24 +1,8 @@
+local simdefs = include("sim/simdefs")
 local cheatmenu = include("fe/cheatmenu")
-local util = include("client_util")
-local sa_util = include(SA_PATH .. '/modulesModify/util')
 
-local oldCheatMenuInit = cheatmenu.menu.init
--- local cheat_item = sa_util.getLocalValue(oldCheatMenuInit, "cheat_item")
-sa_util.debugFunc(sa_util.getLocalValue, oldCheatMenuInit, "cheat_item",5)
-cheatmenu.menu.init = function(self, screen, debugenv)
-    self.screen = screen
-    self.debugenv = debugenv
-    self.listbox = screen.binder.listbox
-    local onItemSelected = sa_util.getLocalValue(oldCheatMenuInit, "onItemSelected")
-    self.listbox.onItemSelected = util.makeDelegate(nil, onItemSelected, self)
-    local onItemClicked = sa_util.getLocalValue(oldCheatMenuInit, "onItemClicked")
-    self.listbox.onItemClicked = util.makeDelegate(nil, onItemClicked, self)
-    self.menuStack = {}
-    self.lastSelection = {}
-    local CHEAT_MENU = sa_util.getLocalValue(oldCheatMenuInit, "CHEAT_MENU")
-    table.insert(CHEAT_MENU.submenu, 1, cheat_item("debug", function()
-        SALog("clicked")
-    end))
-    self:pushMenu(CHEAT_MENU)
-    self:refresh()
-end
+
+local cheat_item = cheatmenu.cheat_item
+table.insert(simdefs.CHEATS, 1, cheat_item("debug", function()
+    SALog("clicked")
+end))
--- scripts/modulesModify/util.lua
+++ scripts/modulesModify/util.lua
@@ -22,7 +22,36 @@ local function debugFunc(fn, ...)
         print("Function executed successfully")
     end
 end
+
+local function printUpvalue(fn, condition, maxDepth, callChain)
+    maxDepth = maxDepth or 3
+    callChain = callChain or (type(condition) == 'string' and condition or 'root')
+
+    local upvName, value
+    local upvIdx = 1
+    repeat
+        upvName, value = debug.getupvalue(fn, upvIdx)
+        upvIdx = upvIdx + 1
+        callChain = callChain .. "->" .. upvName
+
+        if upvName == condition or (type(condition) == "function" and condition(upvName, value)) then
+            SALog(callChain)
+            return value, upvIdx, fn
+        end
+
+        if type(value) == "function" then
+            if maxDepth > 0 then
+                local subV, subIdx, subFn = printUpvalue(value, condition, maxDepth - 1, callChain)
+                if subIdx then
+                    return subV, subIdx, subFn
+                end
+            end
+        end
+    until upvName == nil
+end
+
 return {
     getLocalValue = getLocalValue,
     debugFunc = debugFunc,
+    printUpvalue = printUpvalue,
 }
